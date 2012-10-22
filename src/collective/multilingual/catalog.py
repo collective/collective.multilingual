52c97527105aa909598d1a32dbc6ac313352d6e8
Malthe Borch <mborch@gmail.com>
2012-10-22 15:29:53 +0200

    Added explicit language indexer which prevents acquisition.

--- src/collective/multilingual/catalog.py
+++ src/collective/multilingual/catalog.py
@@ -7,3 +7,8 @@ def translations(obj):
     unwrapped = aq_base(obj)
     return set(getattr(unwrapped, "translations", ()) or ())
 
+
+@indexer(IMultilingual)
+def language(obj):
+    unwrapped = aq_base(obj)
+    return getattr(unwrapped, "language", None)
