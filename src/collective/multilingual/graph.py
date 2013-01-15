collective/collective.multilingual

commit 88116e5c50e5d03c352ebdb977c7c63f5a951965

Made translation lookup more tollarant, however not sure about the correctness

src/collective/multilingual/graph.py
@@ -107,6 +107,11 @@ class MultilingualTranslationGraph(object):
 
         if not IMultilingual.providedBy(obj):
             logger.warn("integrity error; parent not translation-aware.")
+            return
+
+        if not ITranslationGraph.providedBy(obj):
+            logger.warn("Object is not providing translation graph.")
+            return
 
         obj = ITranslationGraph(obj).getCanonicalContent()
         return cache.set(obj, str(IUUID(obj)))

commit 79a6adac48fbe09408fe330a22825e3176cecebb

fix broken translation lookup from non canonical translations. problem introduced in 88116e5c.

src/collective/multilingual/graph.py
@@ -107,14 +107,13 @@ class MultilingualTranslationGraph(object):
 
         if not IMultilingual.providedBy(obj):
             logger.warn("integrity error; parent not translation-aware.")
-            return
 
-        if not ITranslationGraph.providedBy(obj):
-            logger.warn("Object is not providing translation graph.")
-            return
-
-        obj = ITranslationGraph(obj).getCanonicalContent()
-        return cache.set(obj, str(IUUID(obj)))
+        try:
+            obj = ITranslationGraph(obj).getCanonicalContent()
+        except TypeError:
+            logger.warn("Object does not have a translation graph.")
+        else:
+            return cache.set(obj, str(IUUID(obj)))
 
     @cache
     def getParent(self, cache):