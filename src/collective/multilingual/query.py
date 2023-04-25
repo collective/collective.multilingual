c7f3c4add79eb9e3176800deee9bd1828e90809b
Malthe Borch <mborch@gmail.com>
2023-04-25 10:47:37 +0200

    Add missing file

---
+++ src/collective/multilingual/query.py
@@ -0,0 +1,78 @@
+from .interfaces import IBrowserLayer
+from .utils import getSettings
+from .utils import logger
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.CatalogTool import CatalogTool
+from zope.component import ComponentLookupError
+
+
+_searchResults = CatalogTool.searchResults
+_marker = object()
+
+
+def modifier(query):
+    import pdb; pdb.set_trace()
+
+def applyLanguageFilter(site, blacklist, request, kw):
+    lt = getToolByName(site, "portal_languages", None)
+    if lt is None:
+        return
+
+    for query in (request, kw):
+        if query is not None:
+            language = query.pop("Language", _marker)
+            if language == "all":
+                return
+
+            path_query = query.get("path")
+            if isinstance(path_query, dict) and path_query == {"query": ""}:
+                query.pop("path")
+
+            if language is _marker:
+                language = query.get("language", _marker)
+                if language is not _marker:
+                    if language == "all":
+                        del query["language"]
+
+                    return
+            else:
+                query["language"] = language
+
+            if set(query) & blacklist:
+                return
+
+    language = lt.getPreferredLanguage()
+    if language == lt.getDefaultLanguage():
+        default = u""
+    else:
+        default = None
+
+    query["language"] = (language, default)
+
+    # XXX: For path queries that target a path under a language
+    # folder, and if we want to support a list of (language-neutral)
+    # shared or common folders, this would be the place to customize
+    # the path query to include these. This might be an images folder.
+
+
+def searchResults(self, REQUEST=None, **kw):
+    for request in (REQUEST, getattr(self, "REQUEST", None)):
+        if request is not None and IBrowserLayer.providedBy(request):
+            site = self.portal_url.getPortalObject()
+
+            try:
+                settings = getSettings(site)
+            except ComponentLookupError:
+                break
+
+            if settings.enable_catalog_patch:
+                applyLanguageFilter(site, settings.no_filter, REQUEST, kw)
+
+            break
+
+    return _searchResults(self, REQUEST, **kw)
+
+
+#CatalogTool.searchResults = searchResults
+#CatalogTool.__call__ = searchResults
+#logger.info("patched catalog tool to filter on current language.")
